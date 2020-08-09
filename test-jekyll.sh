33734007edbe12814eafc954656b2a47e4b68033
robnagler <github@q33.us>
2020-08-09 22:27:23 +0000

    force kramdown >= 2.3.0

--- test-jekyll.sh
+++ test-jekyll.sh
@@ -4,11 +4,12 @@
 # If you "rm -f Gemfile.lock", it will recreate it.
 #
 docker rm -f jekyll
-docker run -d --name=jekyll --net=host -i --rm -v="$PWD":/srv/jekyll jekyll/jekyll:pages bash <<'EOF'
+# for some reason docker control isn't working. probably busybox or no -t
+trap 'echo exit; exit 1' INT
+trap 'docker rm -f jekyll' EXIT
+docker run --name=jekyll --net=host -i --rm -v="$PWD":/srv/jekyll jekyll/jekyll:pages bash <<'EOF' &
 bundle install
 bundle exec jekyll serve --host 0.0.0.0
 EOF
-echo 'You can:
-
-docker stop jekyll
-'
+echo "Visit http://$(hostname -f):4000"
+wait
